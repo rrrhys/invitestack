7672e4f28e433fcd5734c3d0cbe948ea0b821f8e
Rhys Williams <rrrhys@gmail.com>
2013-01-27 18:02:49 +1100

    ...

--- application/views/header.php
+++ application/views/header.php
@@ -6,6 +6,6 @@
 	<!--
 	<script type="text/javascript" src="http://provisioning.devshopous.dev/scripts/jquery-1.7.2.min.js"></script>
 	 -->
-	 <script type="text/javascript" src="http://code.jquery.com/ui/1.8.0/jquery-ui.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
 	<script type="text/javascript" src="/js/bootstrap.js"></script>
 	<script type="text/javascript" src="/js/site.js?id=<?=Time()?>"></script>
