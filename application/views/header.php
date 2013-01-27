e81e7786572b7c8e357013dc5521da323c90b708
Rhys Williams <rrrhys@gmail.com>
2013-01-27 17:58:24 +1100

    remove local jquery use cdn

--- application/views/header.php
+++ application/views/header.php
@@ -3,6 +3,9 @@
 	<link rel="stylesheet" href="/css/bootstrap.css" />
 	<link rel="stylesheet" href="/css/bootstrap.additional.css" />
 	<link rel="stylesheet" href="/css/bootstrap-responsive.css" />
+	<!--
 	<script type="text/javascript" src="http://provisioning.devshopous.dev/scripts/jquery-1.7.2.min.js"></script>
+	 -->
+	 <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
 	<script type="text/javascript" src="/js/bootstrap.js"></script>
 	<script type="text/javascript" src="/js/site.js?id=<?=Time()?>"></script>
